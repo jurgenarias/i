da8ca3a2933a0c38363110c50b504dddd492588d
jurgenarias <jurgen.as@gmail.com>
2020-11-11 18:46:34 -0500

    tildes

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ function runEnter() {
   d3.select("h1>span").text((inputValue)
   .replace(/a/g,"i").replace(/e/g,"i").replace(/o/g,"i")
   .replace(/u/g,"i").replace(/A/g,"I").replace(/E/g,"I")
-  .replace(/O/g,"I").replace(/U/g,"I"))
+  .replace(/O/g,"I").replace(/U/g,"I").replace(/á/g,"í")
+  .replace(/é/g,"í").replace(/ó/g,"í")
+  .replace(/ú/g,"í").replace(/Á/g,"Í").replace(/É/g,"Í")
+  .replace(/Ó/g,"Í").replace(/Ú/g,"Í"))
 }
