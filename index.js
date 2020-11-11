ed5d147a610efe581facc1312486fdb4fa963a24
jurgenarias <jurgen.as@gmail.com>
2020-11-11 17:22:16 -0500

    again

---
+++ index.js
@@ -0,0 +1,32 @@
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#example-form-input");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Print the value to the console
+  // console.log(inputValue);
+
+  // Set the span tag in the h1 element to the text
+  // that was entered in the form
+  d3.select("h1>span").text((inputValue)
+  .replace(/a/g,"i").replace(/e/g,"i").replace(/o/g,"i")
+  .replace(/u/g,"i").replace(/A/g,"I").replace(/E/g,"I")
+  .replace(/O/g,"I").replace(/U/g,"I"))
+}
