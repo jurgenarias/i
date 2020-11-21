d2631377953e2801747d22a62eaf6a93e5a45853
jurgenarias <jurgen.as@gmail.com>
2020-11-20 19:02:14 -0500

    Que Qui Gue Gui

--- index.js
+++ index.js
@@ -6,11 +6,32 @@ function runEnter() {
   d3.event.preventDefault();
   var inputElement = d3.select("#example-form-input");
   var inputValue = inputElement.property("value");
-  d3.select("h1>span").text((inputValue)
+  
+  var texto = (inputValue)
+  .replace(/gui/g,"!!!@").replace(/gue/g,"!!!@")
+  .replace(/guí/g,"%%%&").replace(/gué/g,"%%%&")
+  .replace(/que/g,"-###").replace(/qui/g,"-###")
+  .replace(/qué/g,"===-").replace(/quí/g,"===-")
+  .replace(/GUI/g,";;;@").replace(/GUE/g,";;;@")
+  .replace(/GUÍ/g,"%%%&").replace(/GUÉ/g,"%%%&")
+  .replace(/QUE/g,"-#-#-").replace(/QUI/g,"-#-#-")
+  .replace(/QUÉ/g,"/=/=/").replace(/QUÍ/g,"&%&%&%")
+  .replace(/Que/g,"@!@!@!")
+  .replace(/Qué/g,"-%-%-%")
   .replace(/a/g,"i").replace(/e/g,"i").replace(/o/g,"i")
   .replace(/u/g,"i").replace(/A/g,"I").replace(/E/g,"I")
   .replace(/O/g,"I").replace(/U/g,"I").replace(/á/g,"í")
   .replace(/é/g,"í").replace(/ó/g,"í")
   .replace(/ú/g,"í").replace(/Á/g,"Í").replace(/É/g,"Í")
-  .replace(/Ó/g,"Í").replace(/Ú/g,"Í"))
+  .replace(/Ó/g,"Í").replace(/Ú/g,"Í")
+  
+  var textoConQ = texto
+  .replace(/!!!@/g,"gui").replace(/%%%&/g,"guí")
+  .replace(/-###/g,"qui").replace(/===-/g,"quí")
+  .replace(/;;;@/g,"GUI").replace(/%%%&/g,"GUÍ")
+  .replace(/-#-#-/g,"QUI").replace(/&%&%&%/g,"QUÍ")
+  .replace(/@!@!@!/g,"Qui").replace(/-%-%-%/g,"Quí")
+
+  
+  d3.select("h1>span").text(textoConQ)
 }
